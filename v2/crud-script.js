b24f4be4ab3c94c10366c809c084015da11141fc
RemoroLiz <165878879+RemoroLiz@users.noreply.github.com>
2026-08-07 01:35:06 +0700

    Update crud-script.js

--- v2/crud-script.js
+++ v2/crud-script.js
@@ -3,7 +3,7 @@
 // ==============================
 const CONFIG = {
   // GANTI DENGAN URL WEB APP APPS SCRIPT ANDA SETELAH DEPLOY (lihat Code.gs)
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw1vBvut6FwYvOMB0s1Tr5OdjJwV0ThJLkNHB1PP-dleOGj_dhh5lQdIBpOPdnWC4JT/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbymZh-zk9R_toZ4zyPzCfGFqCcXICZVpXVn4G4KTaPTIp1va4_24BAMHN78qf7OcvK_0Q/exec",
   PAGE_SIZE: 15,
   MAX_IMAGES: 5,
 };
