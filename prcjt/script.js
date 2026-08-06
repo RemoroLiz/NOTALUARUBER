f5dfa0b80197094c10c304c8c4dfb55092c69b1d
RemoroLiz <165878879+RemoroLiz@users.noreply.github.com>
2026-08-06 18:06:57 +0700

    Update script.js

--- prcjt/script.js
+++ prcjt/script.js
@@ -3,7 +3,7 @@
 // ==============================
 const CONFIG = {
   // GANTI DENGAN URL WEB APP APPS SCRIPT ANDA SETELAH DEPLOY (lihat Code.gs)
- WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw1vBvut6FwYvOMB0s1Tr5OdjJwV0ThJLkNHB1PP-dleOGj_dhh5lQdIBpOPdnWC4JT/exec",
+ WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzvftbTX0lsO9ly8fg5gKECpuvk7-VYmylm2UDg0UlVINMxiSkQWeKWRLuniwfI7aIN/exec",
   MAX_IMAGES: 5,
   MAX_IMAGE_DIMENSION: 1280, // px, sisi terpanjang setelah kompresi
   IMAGE_QUALITY: 0.7, // kualitas JPEG hasil kompresi
@@ -326,7 +326,7 @@ function buildStoreReceipt(id, v) {
 
   return `
     <div class="thermal-receipt">
-      <div class="tr-title">LAPORAN TOKO</div>
+      <div class="tr-title">LAPORAN NOTA LUAR UBER</div>
       <div class="tr-sub">${id}</div>
       <hr />
       <div class="tr-columns">
