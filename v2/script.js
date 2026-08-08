c83300c216aefd4142e93714effac94508d8500a
RemoroLiz <165878879+RemoroLiz@users.noreply.github.com>
2026-08-08 14:01:49 +0700

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -3,7 +3,7 @@
 // ==============================
 const CONFIG = {
   // GANTI DENGAN URL WEB APP APPS SCRIPT ANDA SETELAH DEPLOY (lihat Code.gs)
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw1vBvut6FwYvOMB0s1Tr5OdjJwV0ThJLkNHB1PP-dleOGj_dhh5lQdIBpOPdnWC4JT/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxF64rJ5hoOYV084Y2MDUOs9dia8TxIKBmEYLptfG1bPdEnICRDaVkR4YcWg-umzuNY/exec",
   MAX_IMAGES: 8,
   MAX_IMAGE_DIMENSION: 1280, // px, sisi terpanjang setelah kompresi
   IMAGE_QUALITY: 0.7, // kualitas JPEG hasil kompresi
