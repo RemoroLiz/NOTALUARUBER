e696e675c4d6e0d72a39b4e719e0f33066dc0ac0
RemoroLiz <165878879+RemoroLiz@users.noreply.github.com>
2026-08-10 23:24:56 +0700

    Update customer-script.js

--- final/customer-script.js
+++ final/customer-script.js
@@ -3,7 +3,7 @@
 // ==============================
 const CONFIG = {
   // GANTI DENGAN URL WEB APP APPS SCRIPT ANDA SETELAH DEPLOY (lihat Code.gs)
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwNo7q2D8oHWp7U75xxfQE4eRk6TeBeOxlFDixb0-XQUPgPIuyAI6mfcz0BXGvGQ4Mt/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx24SIxySt-uGMnkhMuS7SySaTFown7uQlRqZQTF0AgCTR9kQkVchRboibvRDDHfXv7/exec",
   MAX_IMAGE_DIMENSION: 1280, // px, sisi terpanjang setelah kompresi
   IMAGE_QUALITY: 0.7, // kualitas JPEG hasil kompresi
   MAX_CUSTOMER_PHOTOS: 3,
